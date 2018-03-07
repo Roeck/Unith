7826ba78a31e59764731fa78d2d2e7f9bcb12c6e
rons9n <rons9n@gmail.com>
2018-03-07 18:28:13 -0500

    layout-2-2nd

---
+++ app/src/main/java/com/example/rons9n/unith/UnithActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rons9n.unith
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UnithActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unith2)
+    }
+}
