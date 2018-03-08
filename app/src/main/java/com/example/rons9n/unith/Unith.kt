8850f4ff62ffa6e6532ef88630083450f6f81daf
rons9n <rons9n@gmail.com>
2018-03-07 22:06:36 -0500

    layout transition

--- app/src/main/java/com/example/rons9n/unith/Unith.kt
+++ app/src/main/java/com/example/rons9n/unith/Unith.kt
@@ -1,12 +1,19 @@
 package com.example.rons9n.unith
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_unith.*
 
 class Unith : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_unith)
+
+        getStartedButton.setOnClickListener {
+            val leagueIntent = Intent(this, Unith::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
